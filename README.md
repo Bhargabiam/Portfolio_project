d9808bf61068db603657c3b532013d763cd11b27
Bhargabiam <87924439+Bhargabiam@users.noreply.github.com>
2023-11-30 23:33:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Portfolio_project
+ my portfolio project
---
+++ js/script.js
@@ -0,0 +1,25 @@
+/**
+ * @copyright bharagbChatterjee 2023
+ * @author Sadee <bhargabiam@gmail.com>
+ */
+
+"use strict";
+
+// Light and dark mode
+
+const $themeBtn = document.querySelector("[data-theme-btn]");
+const $HTML = document.documentElement;
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (sessionStorage.getItem("theme")){
+    $HTML.dataset.theme = sessionStorage.getItem("theme");
+}else{
+    $HTML.dataset.theme = isDark? "dark" : "light";
+}
+
+const changeTheme = ()=>{
+    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+    sessionStorage.setItem("theme", $HTML.dataset.theme);
+}
+
+$themeBtn.addEventListener("click", changeTheme);

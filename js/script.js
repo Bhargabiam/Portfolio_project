d035cb3ce32b6e0b82d6484bda62dc9c8d31cc3c
Bhargabiam <87924439+Bhargabiam@users.noreply.github.com>
2023-12-01 18:22:36 +0530

    favicon and logo change

--- js/script.js
+++ js/script.js
@@ -7,19 +7,20 @@
 
 // Light and dark mode
 
-const $themeBtn = document.querySelector("[data-theme-btn]");
-const $HTML = document.documentElement;
+const themeBtn = document.querySelector("[data-theme-btn]");
+const hbody = document.documentElement;
 let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-if (sessionStorage.getItem("theme")){
-    $HTML.dataset.theme = sessionStorage.getItem("theme");
-}else{
-    $HTML.dataset.theme = isDark? "dark" : "light";
+if (sessionStorage.getItem("theme")) {
+  hbody.dataset.theme = sessionStorage.getItem("theme");
+} else {
+  hbody.dataset.theme = isDark ? "dark" : "light";
 }
 
-const changeTheme = ()=>{
-    $HTML.dataset.theme = sessionStorage.getItem("theme") === "light" ? "dark" : "light";
-    sessionStorage.setItem("theme", $HTML.dataset.theme);
-}
+const changeTheme = () => {
+  hbody.dataset.theme =
+    sessionStorage.getItem("theme") === "light" ? "dark" : "light";
+  sessionStorage.setItem("theme", hbody.dataset.theme);
+};
 
-$themeBtn.addEventListener("click", changeTheme);
+themeBtn.addEventListener("click", changeTheme);

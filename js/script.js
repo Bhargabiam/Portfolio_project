63172f56f7f7ff428d10498540bde84c4d9df1bf
Bhargabiam <87924439+Bhargabiam@users.noreply.github.com>
2023-12-02 23:00:14 +0530

    emailJS implimentation, profile picture change

--- js/script.js
+++ js/script.js
@@ -46,3 +46,30 @@ tabBtn.forEach((item) => {
     lastActiveTabBtn = this;
   });
 });
+
+// send email 
+
+function sendEmail () {
+  var templateParams = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+};
+
+emailjs.send('service_kzuropc', 'template_ulby5d7', templateParams)
+    .then((response) => {
+      document.getElementById("name").value = "";
+      document.getElementById("email").value = "";
+      document.getElementById("subject").value = "";
+      document.getElementById("message").value = "";
+      console.log(response)
+    }, function(error) {
+       console.log('FAILED...', error);
+    });
+
+}
+document.getElementById("email-send").addEventListener("submit", function(event) {
+  event.preventDefault(); 
+  sendEmail(); 
+});
